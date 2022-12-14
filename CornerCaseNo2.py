78bf0331f9a9f0a5152e79333a0e39312348cb3f
domssab <saballo.chantaldominique@gmail.com>
2022-12-14 18:04:01 +0800

    messages enqueue

--- CornerCaseNo2.py
+++ CornerCaseNo2.py
@@ -10,3 +10,6 @@ hazard_lights = Message("Hazard lights turned on")
 
 wipers < hazard_lights
 
+messages = PriorityQueue()
+messages.enqueue_with_priority(CRITICAL, wipers)
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
