7e941f610eeda16ab490d6f735f87d2feaac0871
domssab <saballo.chantaldominique@gmail.com>
2022-12-14 18:03:17 +0800

    class message

--- CornerCaseNo2.py
+++ CornerCaseNo2.py
@@ -4,12 +4,3 @@ from dataclasses import dataclass
 class Message:
     event: str
 
-
-wipers = Message("Windshield wipers turned on")
-hazard_lights = Message("Hazard lights turned on")
-
-wipers < hazard_lights
-
-messages = PriorityQueue()
-messages.enqueue_with_priority(CRITICAL, wipers)
-messages.enqueue_with_priority(IMPORTANT, hazard_lights)
